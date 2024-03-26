f6295c114e1ba2080aed05b7a7ba5eecf2b877a2
Carlos Barbosa <Carlos.Barbosa@prodeb.ba.gov.br>
2024-03-25 22:34:35 -0300

    arquivo incial com o valor por parametro

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Olá Mundo"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id" : item_id}
